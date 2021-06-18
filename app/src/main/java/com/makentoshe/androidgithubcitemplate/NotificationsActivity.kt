b64bc5bedb76b225f2d7509f37f4ca75214f39ed
torin1489 <t.metelew@gmail.com>
2021-06-18 17:11:59 +0300

    Add prompt_noti.xml Add AlertDialog to NotificationsActivity.kt

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/NotificationsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/NotificationsActivity.kt
@@ -1,15 +1,19 @@
 package com.makentoshe.androidgithubcitemplate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class NotificationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +22,42 @@ class NotificationsActivity : AppCompatActivity() {
 
         val notis = findViewById<RecyclerView>(R.id.notis)
         notis.layoutManager = LinearLayoutManager(this)
-        notis.adapter = CustomRecyclerAdapter(5)
+        notis.adapter = CustomRecyclerAdapter(10)
+
+        val add_button = findViewById<Button>(R.id.noti_add_button)
+        add_button.setOnClickListener {
+            val li: LayoutInflater = LayoutInflater.from(this)
+            val promptsView: View = li.inflate(R.layout.prompt_noti, null)
+
+            //Создаем AlertDialog
+            val mDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+            //Настраиваем prompt.xml для нашего AlertDialog:
+            mDialogBuilder.setView(promptsView)
+
+            //Настраиваем отображение поля для ввода текста в открытом диалоге:
+            val time_input = findViewById<EditText>(R.id.noti_pro_time_input)
+            val text_input = findViewById<EditText>(R.id.noti_pro_text_input)
+
+            mDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Сохранить") { dialog, id ->
+                    if (text_input != null) {
+                        add_button.setText(text_input.text)
+                    }
+                }                                                                                     //TODO: Save text
+                .setNegativeButton("Отмена") { dialog, id -> dialog.cancel()}
+
+            //Настраиваем сообщение в диалоговом окне:*/
+            val alertDialog: AlertDialog = mDialogBuilder.create()
+            alertDialog.show()
+
+
+        }
     }
 }
 
 class CustomRecyclerAdapter(val strings: Int): RecyclerView.Adapter<MyViewHolder>() {
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_notifications, parent, false)
         return MyViewHolder(itemView)
