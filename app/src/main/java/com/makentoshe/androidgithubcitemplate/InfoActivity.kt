ad26c41fb27350cc1544edf5b702519995164c73
torin1489 <t.metelew@gmail.com>
2021-06-19 19:24:55 +0300

    duno

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/InfoActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/InfoActivity.kt
@@ -3,7 +3,7 @@ package com.makentoshe.androidgithubcitemplate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class InfoActivity : AppCompatActivity() {
+class   InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
